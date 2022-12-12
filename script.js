a606511333ff347a78919c78bfb51a2e17d24203
Alexandr <kobal2009@yandex.ru>
2022-12-13 03:39:21 +0500

    Добавил исправления с выходом из игры

--- script.js
+++ script.js
@@ -9,66 +9,56 @@ let newGame
 const game = function(randomNumber) {   
     //Загадываем число
     randomNumber = Math.floor(Math.random() * 100) + 1
-    //Подсказка загаданного числа в консоле
-    console.log(randomNumber)   
     let count = 10
     function asking() {
         guesses = +prompt('Угадай число от 1 до 100, у тебя ' + count + ' попыток')
         //Логика счетчика попыток
         count--
         if (count === 0) {
-            alert ('Попытки закончились, ты проиграл')
-            newGame = confirm('Сыграем еще?')
-            if(newGame) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
                 return game()
-                } else {
-                    guesses = null
-                    alert('Пока')
-                    return
-                }
+            } else {
+                alert('Игра окончена')
+                return
+            }
         }
         //Проверка поля ввода
         while (isNaN(guesses)) {
             alert('Введите число!')
             count++
             asking()
+            return
         }
         while (!guesses) {
-            alert('Игра окончена')
-            newGame = confirm('Сыграем еще?')
-            if(newGame) {
+            if (confirm('Игра закончена, хотите сыграть еще?')) {
                 return game()
-                } else {
-                    guesses = null
-                    alert('Пока')
-                    return
-                } 
+            } else {
+                alert('Игра окончена')
+                return
+            }
         }
         //Сравнение ответа с загадкой
         while (guesses > randomNumber) {
             alert('Загаданное число меньше, осталось ' + count + ' попыток')
             asking()
+            return
         } 
         while (guesses < randomNumber) {
             alert('Загаданное число больше, осталось ' + count + ' попыток')
             asking()
+            return
         }
-        if (guesses === randomNumber) {
-            alert('Ты победил')
-            newGame = confirm('Сыграем еще?')
-            if(newGame) {
+        while (guesses === randomNumber) {
+            if (confirm('Ты победил, хотите сыграть еще?')) {
                 return game()
-                } else {
-                    guesses = null
-                    alert('Пока')
-                    return
-                } 
-        }
-        
+            } else {
+                alert('Игра окончена')
+                return
+            }
+        }       
     }
     asking()
 }
-
 game(randomNumber)
 
 
